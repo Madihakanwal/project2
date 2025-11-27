296c169a625f808ced83134d379cf46e61ea7963
madiha <madihakanwal655@gmail.com>
2025-11-27 10:39:56 +0500

    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\UserSeeder;
+use Database\Seeders\PaymentSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,21 +18,22 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(UserSeeder::class);
-         $this->call(CartItemSeeder::class);
-          $this->call(CartSeeder::class);
-           $this->call(CategorySeeder::class);
-            $this->call(CouponSeeder::class);
-             $this->call(CustomerSeeder::class);
-              $this->call(OrderItemSeeder::class);
-               $this->call(OrderSeeder::class);
-               $this->call(ProductSeeder::class);
-               $this->call(ProductImageSeeder::class);
-               $this->call(ProductVariantSeeder::class);
-               $this->call(ReviewSeeder::class);
-               $this->call(SettingSeeder::class);
-               $this->call(ShippingAddressSeeder::class);
-               $this->call(SubCategorySeeder::class);
-               $this->call(WishlistSeeder::class);
-$this->call(PaymentSeeder::class);
+        $this->call(CartItemSeeder::class);
+        $this->call(CartSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(CouponSeeder::class);
+        $this->call(CustomerSeeder::class);
+        $this->call(OrderItemSeeder::class);
+        $this->call(OrderSeeder::class);
+        $this->call(ProductSeeder::class);
+        $this->call(ProductImageSeeder::class);
+        $this->call(ProductVariantSeeder::class);
+        $this->call(ReviewSeeder::class);
+        $this->call(SettingSeeder::class);
+        $this->call(ShippingAddressSeeder::class);
+        $this->call(SubCategorySeeder::class);
+        $this->call(WishlistSeeder::class);
+        $this->call(PaymentSeeder::class);
+
     }
 }
